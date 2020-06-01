b8921fb1fb3b255e00d194a1e4b3d10028e59fbf
Kevin <Atnnetworkk@gmail.com>
2020-06-01 22:16:27 +0200

    Updates

---
+++ precache-manifest.37ea532bc113eb060b23ffd86a7991ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16204a8b0006012e943ba3e44f25e762",
+    "url": "/portfollio/index.html"
+  },
+  {
+    "revision": "b8690408cef01e2277e9",
+    "url": "/portfollio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "723f6e5fb6d5c28c4d27",
+    "url": "/portfollio/static/js/2.cae6e0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfollio/static/js/2.cae6e0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8690408cef01e2277e9",
+    "url": "/portfollio/static/js/main.73a7d1c0.chunk.js"
+  },
+  {
+    "revision": "12d5bfb833338ed93cd7",
+    "url": "/portfollio/static/js/runtime-main.e257fda0.js"
+  }
+]);
