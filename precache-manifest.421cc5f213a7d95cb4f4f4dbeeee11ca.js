1242b516e809e43728b07822d415e8c719fe3f0d
Atndesign <atnnetworkk@gmail.com>
2021-05-16 17:41:00 +0000

    Deploying to gh-pages from @ Atndesign/portfollio@8026a4a4af2ebd8dd6208cddf5f5f8554da3d8ad 🚀

---
+++ precache-manifest.421cc5f213a7d95cb4f4f4dbeeee11ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edef43590a46cb77cc3bb13b7ea3b212",
+    "url": "/portfollio/index.html"
+  },
+  {
+    "revision": "64e9a8d8a18eeca51c77",
+    "url": "/portfollio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "723f6e5fb6d5c28c4d27",
+    "url": "/portfollio/static/js/2.cae6e0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfollio/static/js/2.cae6e0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e9a8d8a18eeca51c77",
+    "url": "/portfollio/static/js/main.6aa344cb.chunk.js"
+  },
+  {
+    "revision": "12d5bfb833338ed93cd7",
+    "url": "/portfollio/static/js/runtime-main.e257fda0.js"
+  }
+]);
